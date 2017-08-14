34e95f388d3ace12503eaee81bdd25489c5dc015
HauJie <haujiechan@gmail.com>
2017-08-14 18:56:37 +0800

    done

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
+var start;
+
 function addRestaurantListener(){
   startBtn = document.getElementById('start-random-button');
-  
-  // Replace 'functionName' with the name of the function that you have written
-  startBtn.addEventListener("click", functionName)
+  startBtn.addEventListener("click", function(){
+  	  start = setInterval(randomFood,200);
+  	  if(!document.getElementById('word')){
+		restaurantString = wordCreate();
+		getBtn = document.getElementById('get-restaurant-button');
+		getBtn.parentNode.insertBefore(restaurantString,getBtn.nextSibling);
+	  }
+	  restaurantString.style.display = 'none'
+	  getBtn = document.getElementById('get-restaurant-button');
+	  getBtn.addEventListener("click", getRestaurant);
+  })
+}
 
-  getBtn = document.getElementById('get-restaurant-button');
-  
-  // Replace 'functionName' with the name of the function that you have written
-  getBtn.addEventListener("click", functionName);
+function getRestaurant(){
+	clearInterval(start)
+	var restaurantName = document.getElementById('restaurant').innerHTML;
+	var restaurantString = document.getElementById('word')
+	restaurantString.innerHTML = "Great, lets eat " + restaurantName + "!!!"
+	restaurantString.style.display = "inline"
+}
+
+function wordCreate(){
+	createWord = document.createElement('p');
+	createWord.setAttribute('id','word');
+	return createWord
+}
+
+function randomFood(){
+	restaurantArr = ["nasi kukus", "fish head noodle", "uncle don", "chicken rice"]
+	var randomNumber = Math.floor(Math.random()*restaurantArr.length)
+	document.getElementById('restaurant').innerHTML = restaurantArr[randomNumber]
 }
 
 // wait for DOM content to load before binding an event
